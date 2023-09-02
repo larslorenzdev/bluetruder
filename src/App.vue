c1fdc1d63b38c03538d154ee4c70f392f3dcf023
Lars Lorenz <mail@larslorenz.dev>
2023-09-02 10:19:53 +0200

    downgrade to yarn 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
 
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Label Generator</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue'
 
-</style>
+const drawer = ref(null)
+</script>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+}
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue()
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [require('autoprefixer')],
+    },
+  },
+  server: {
+    strictPort: true,
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  preview: {
+    strictPort: true,
+    port: 3000,
+    host: '0.0.0.0',
+  },
 })
